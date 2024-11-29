8e22d473b62b328459de9baf68426f1f7be25db0
itssuffian <suffianwork02@gmail.com>
2024-11-29 10:27:49 +0800

    Modified name on variable 'firebaseConfig'

--- src/config/FirebaseConfiguration.ts
+++ src/config/FirebaseConfiguration.ts
@@ -1,17 +1,17 @@
 // Import all functions needed
-import {initializeApp} from 'firebase/app';
-import {getAuth} from 'firebase/auth';
-import {getFirestore} from 'firebase/firestore';
-import {getStorage} from 'firebase/storage';
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 // Firebase configuration
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
     authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,10 +1,10 @@
 interface ImportMetaEnv {
     readonly VITE_FIREBASE_API_KEY: string;
     readonly VITE_FIREBASE_AUTH_DOMAIN: string;
-    readonly VITE_FIREBASE_PROJECT_I: string;
-    readonly VITE_FIREBASE_STORAGE_BUCKET: string;
-    readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string;
-    readonly VITE_FIREBASE_APP_ID: string;
+    readonly VITE_APP_FIREBASE_PROJECT_I: string;
+    readonly VITE_APP_FIREBASE_STORAGE_BUCKET: string;
+    readonly VITE_APP_FIREBASE_MESSAGING_SENDER_ID: string;
+    readonly VITE_APP_FIREBASE_APP_ID: string;
 }
 
 interface ImportMeta {
